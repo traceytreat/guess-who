059df955f2c5126f26800c09c262c4ed84b61cbc
Tracey <tracey@Traceys-Air.home>
2022-11-01 01:34:11 -0500

    set up and display shuffled array

--- client.js
+++ client.js
@@ -1,1 +1,58 @@
-console.log('Here are all the available people:', people);
+$(document).ready(onReady);
+
+
+//console.log('Here are all the available people:', people);
+
+let current = 0;
+
+function onReady(){
+
+    $('#people').on('click', '.person', checkAnswer);
+    render();
+}
+
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+function shuffleArr(){
+    // this can be done with the fisher-yates algorithm
+    // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+
+    for (i = people.length - 1; i > 0; i--){
+        let random = Math.floor(Math.random() * (i + 1));
+        let temp = people[i];
+        people[i] = people[random];
+        people[random] = temp;
+    }
+}
+
+function checkAnswer(){
+    //console.log('clicked:', $(this).data().name);
+    if ($(this).data().name === people[current].name){
+        console.log('correct');
+        render();
+    } else {
+        console.log('incorrect');
+    }
+}
+
+function render(){
+    // first shuffle the array and then select a random person
+    shuffleArr();
+    current = randomNumber(0, people.length-1);
+    $('h1').empty();
+    $('h1').append(`
+        Click on: ${people[current].name}
+    `);
+
+    // display all people
+    $('#people').empty();
+    for (let person of people){
+        $('#people').append(`
+            <span class="person" data-name="${person.name}">
+                <img src="https://github.com/${person.githubUsername}.png?size=150" alt="Profile image of ${person.name}">
+            </span>
+        `);
+    }
+}
--- data.js
+++ data.js
@@ -27,4 +27,53 @@ var people = [
     name: 'Chris',
     githubUsername: 'christopher-black'
   },
+  // i only added the people who had profile pics set up (sorry everyone else):
+  {
+    name: 'Tracey',
+    githubUsername: 'traceytreat'
+  },
+  {
+    name: 'Andrew',
+    githubUsername: 'arcarey'
+  },
+  {
+    name: 'Ozzy',
+    githubUsername: 'ozzythecoder'
+  },
+  {
+    name: 'Seth',
+    githubUsername: 'SKPyle'
+  },
+  {
+    name: 'Anthony',
+    githubUsername: 'AnthonyDampier'
+  },
+  {
+    name: 'Paul',
+    githubUsername: 'pn0r'
+  },
+  {
+    name: 'Paolo',
+    githubUsername: 'pdebuque'
+  },
+  {
+    name: 'Dylan',
+    githubUsername: 'dylanroets'
+  },
+  {
+    name: 'Brett',
+    githubUsername: 'brg078'
+  },
+  {
+    name: 'Victoria',
+    githubUsername: 'victoriamertens'
+  },
+  {
+    name: 'Billy',
+    githubUsername: 'WSalinas315'
+  },
+  {
+    name: 'Jared',
+    githubUsername: 'jared-lindquist'
+  },
 ];
